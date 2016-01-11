309642e0409e7887d9ea4eefb73c29772e632e56
Connor W <mwilkinson@ggc.edu>
2016-01-11 18:29:49 +0000

    WOOAGAIN

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to my webpage.</title>
+    </head>
+    <body>
+        <h1> HELLO WORLD</h1>
+        <p>I am a webpage.</p>
+        <p><php? 
+        
+        echo time();></p>
+    </body>
+</html>
